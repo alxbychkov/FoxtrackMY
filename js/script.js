91a2643dedf09a7089a9146baa3b4cc6d6d8f4cf
Alexander Bychkov <alx.bychkov@gmail.com>
2023-04-26 19:23:26 +0300

    feature: add collapse to tracklist

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
-new Swiper('.image-slider', {
-	spaceBetween: 10,
-	loop: true,
-	navigation: {
-		nextEl: '.carousel-control-next',
-		prevEl: '.carousel-control-prev'
-	},
-	pagination: {
-		el: '.carousel-indicators',
-		clickable: true,
-		type: 'bullets'
-	},
-	breakpoints: {
-		320: {
-			slidesPerView: 2
+if (window.Swiper) {
+	new Swiper('.image-slider', {
+		spaceBetween: 10,
+		loop: true,
+		navigation: {
+			nextEl: '.carousel-control-next',
+			prevEl: '.carousel-control-prev'
 		},
-		991: {
-			slidesPerView: 3
+		pagination: {
+			el: '.carousel-indicators',
+			clickable: true,
+			type: 'bullets'
 		},
-		1200: {
-			slidesPerView: 5
+		breakpoints: {
+			320: {
+				slidesPerView: 2
+			},
+			991: {
+				slidesPerView: 3
+			},
+			1200: {
+				slidesPerView: 5
+			}
 		}
-	}
-});
+	});
+	
+	new Swiper('.image-slider-adv', {
+		slidesPerView: 1,
+		spaceBetween: 10,
+		loop: true,
+		autoplay: {
+			delay: 4000
+		},
+		pagination: {
+			el: '.carousel-indicators',
+			clickable: true,
+			type: 'progressbar'
+		}
+	});
+}
 
-new Swiper('.image-slider-adv', {
-	slidesPerView: 1,
-	spaceBetween: 10,
-	loop: true,
-	autoplay: {
-		delay: 4000
-	},
-	pagination: {
-		el: '.carousel-indicators',
-		clickable: true,
-		type: 'progressbar'
-	}
-});
+$('.collapse').collapse()
